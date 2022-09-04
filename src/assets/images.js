f3456297dcba9b24d9ce04be53df4f3dec510927
Farah Yasser <farah.yasser@archerirm.com>
2022-09-05 01:18:16 +0200

    Adding icon assets

---
+++ src/assets/images.js
@@ -0,0 +1,11 @@
+const TrashIcon = require('./icons/trash.png');
+const EarthIcon = require('./icons/earth.png');
+const doneIcon = require('./icons/done.png');
+
+const Images = {
+  trash: TrashIcon,
+  earth: EarthIcon,
+  done: doneIcon,
+};
+
+export default Images;
