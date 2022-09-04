fed3e2c50566937cb4ae3bbe1b3d86707a87b224
Farah Yasser <farah.yasser@archerirm.com>
2022-09-05 01:19:12 +0200

    Updating Header styling

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,5 +3,11 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   container: {
     alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+  text: {
+    fontSize: 24,
   },
 });
