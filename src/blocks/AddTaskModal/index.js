0da600208be56cea40b9a3290aab5a931789280f
Farah Yasser <farah.yasser@archerirm.com>
2022-09-05 02:19:04 +0200

    Creating Modal for adding a new task

---
+++ src/blocks/AddTaskModal/index.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import Modal from 'react-native-modal';
+import {Button, Input} from '../../components';
+import styles from './styles';
+import PropTypes from 'prop-types';
+
+const AddTaskModal = ({isVisible, onBackDropPress, onTaskSubmit}) => {
+  const [todoTitle, setTodoTitle] = useState('');
+  const onChangeTitle = txt => setTodoTitle(txt);
+  const onAddButtonPress = () => {
+    onTaskSubmit(todoTitle);
+    setTodoTitle('');
+  };
+  return (
+    <Modal
+      isVisible={isVisible}
+      onBackdropPress={onBackDropPress}
+      backdropOpacity={0.6}
+      animationIn="zoomInDown"
+      animationOut="zoomOutUp"
+      animationInTiming={600}
+      animationOutTiming={600}
+      backdropTransitionInTiming={600}
+      backdropTransitionOutTiming={600}>
+      <View style={styles.container}>
+        <Input
+          placeholder={'ADD_TASK'}
+          value={todoTitle}
+          onChangeValue={onChangeTitle}
+          autoFocus={true}
+        />
+        <Button title={'ADD'} onPress={onAddButtonPress} />
+      </View>
+    </Modal>
+  );
+};
+
+AddTaskModal.PropTypes = {
+  isVisible: PropTypes.bool,
+  onBackDropPress: PropTypes.func,
+  onTaskSubmit: PropTypes.func,
+};
+
+export default AddTaskModal;
---
+++ src/blocks/AddTaskModal/styles.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    padding: 22,
+    backgroundColor: 'white',
+    borderRadius: 12,
+  },
+});
