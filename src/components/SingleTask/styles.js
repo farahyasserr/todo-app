2b1999d901981e09d41177cfde533e6189cdabe0
Farah Yasser <farah.yasser@archerirm.com>
2022-09-05 03:07:33 +0200

    Updating Single Task Component Styling and adding animation

--- src/components/SingleTask/styles.js
+++ src/components/SingleTask/styles.js
@@ -9,27 +9,32 @@ export default StyleSheet.create({
     borderRadius: 12,
     paddingHorizontal: 16,
     marginVertical: 12,
-    paddingVertical: 24,
+    paddingVertical: 20,
     justifyContent: 'space-between',
   },
   text: {
     color: colors.black,
     fontWeight: '500',
+    fontSize: 15,
+    maxWidth: '80%',
   },
   doneText: {
     color: colors.textGrey,
     textDecorationLine: 'line-through',
+    fontSize: 15,
+    maxWidth: '80%',
   },
   deleteIcon: {
-    height: 20,
-    width: 20,
+    height: 24,
+    width: 24,
   },
   doneIcon: {
     height: 20,
     width: 20,
-    marginEnd: 12,
+    marginEnd: 8,
   },
   iconsContainer: {
     flexDirection: 'row',
+    alignItems: 'center',
   },
 });
